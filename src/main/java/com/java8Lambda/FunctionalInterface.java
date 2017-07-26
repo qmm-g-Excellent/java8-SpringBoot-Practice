578d6e74b78483121eb3ab1a39fa909d38d77c21
qmm-g-Excellent <qmmahbc@163.com>
2017-07-26 16:55:29 +0800

    add functionalInterface class

---
+++ src/main/java/com/java8Lambda/FunctionalInterface.java
@@ -0,0 +1,35 @@
+package com.java8Lambda;
+
+/**
+ * Created by qmm on 17-7-26.
+ */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FunctionalInterface {
+
+    public static void main(String args[]) {
+
+        //将一个数组转化为一个List对象，这个方法会返回一个ArrayList类型的对象
+        List<Integer> list = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        System.out.println("All of the number!");
+        eval(list, n -> true);
+
+        System.out.println("Even number!");
+        eval(list, n -> n % 2 == 0);
+
+        System.out.println("Number that greater than 5!");
+        eval(list, n -> n > 5);
+    }
+
+    public static void eval(List<Integer> list, Predicate<Integer> predicate) {
+        for (Integer n : list) {
+            if (predicate.test(n)) {
+                System.out.println(n);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/java8Lambda/MethodReference.java
@@ -0,0 +1,24 @@
+package com.java8Lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by qmm on 17-7-26.
+ */
+public class MethodReference {
+    public static void main(String[] args){
+
+        //List是一个接口,List有两个重要的实现类：ArrayList 和 LinkedList
+        //ArrayList: 动态数组，能够自动增长容量的数组。
+        List names = new ArrayList();
+
+        //向数组中添加数据
+        names.add("peter");
+        names.add("Qmm");
+        names.add("Linda");
+
+        //通过System.out::println引用了输出的方法
+        names.forEach(System.out::println);
+    }
+}
